aaf8aaa0cd9b43fc69917bf7eb4b94661fb8b85b
Shreyans Jain <117734509+ShreyansJain04@users.noreply.github.com>
2024-11-02 15:20:54 +0000

    commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import importlib
 import os
 import cv2
 
+from src.DarthVader.post_proc import process_folder
 
 
 ### Change path to images here
@@ -15,27 +16,27 @@ all_submissions = glob.glob('./src/*')
 os.makedirs('./results/', exist_ok=True)
 for idx,algo in enumerate(all_submissions):
     print('****************\tRunning Awesome Stitcher developed by: {}  | {} of {}\t********************'.format(algo.split(os.sep)[-1],idx+1,len(all_submissions)))
-    try:
-        module_name = '{}_{}'.format(algo.split(os.sep)[-1],'stitcher')
-        filepath = '{}{}stitcher.py'.format( algo,os.sep,'stitcher.py')
-        spec = importlib.util.spec_from_file_location(module_name, filepath)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        PanaromaStitcher = getattr(module, 'PanaromaStitcher')
-        inst = PanaromaStitcher()
 
-        ###
-        for impaths in glob.glob(path):
-            print('\t\t Processing... {}'.format(impaths))
-            stitched_image, homography_matrix_list = inst.make_panaroma_for_images_in(path=impaths)
+    module_name = '{}_{}'.format(algo.split(os.sep)[-1],'stitcher')
+    filepath = '{}{}stitcher.py'.format( algo,os.sep,'stitcher.py')
+    spec = importlib.util.spec_from_file_location(module_name, filepath)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    PanaromaStitcher = getattr(module, 'PanaromaStitcher')
+    inst = PanaromaStitcher()
 
-            outfile =  './results/{}/{}.png'.format(impaths.split(os.sep)[-1],spec.name)
-            os.makedirs(os.path.dirname(outfile),exist_ok=True)
-            cv2.imwrite(outfile,stitched_image)
-            print(homography_matrix_list)
-            print('Panaroma saved ... @ {}'.format(outfile))
-            print('\n\n')
+    ###
+    for impaths in glob.glob(path):
+        print('\t\t Processing... {}'.format(impaths))
+        stitched_image, homography_matrix_list = inst.make_panaroma_for_images_in(path=impaths)
 
-    except Exception as e:
-        print('Oh No! My implementation encountered this issue\n\t{}'.format(e))
+        outfile =  './results/{}/{}.png'.format(impaths.split(os.sep)[-1],spec.name)
+        os.makedirs(os.path.dirname(outfile),exist_ok=True)
+        cv2.imwrite(outfile,stitched_image)
+        print(homography_matrix_list)
+        print('Panaroma saved ... @ {}'.format(outfile))
         print('\n\n')
+
+    
+results= './results'
+process_folder(results)
